f87375a7d5154341d50b9faf8ed67a29af4841cd
Taita54 <giovidime@gmail.com>
2026-03-29 08:16:12 +0200

    Update database configuration in secrets.php

--- App/secrets.php
+++ App/secrets.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'DB_NAME' => ENV==='develod'?'gdmsoftandpict':'gdmsoftandpict',
-    'DB_USER' => 'root',
-    'DB_PASSWORD' => '&a4GUz2z2i1No0&',
-    'driver' => 'mysql',
+    'DB_NAME' => ENV==='develod'?'t':'t',
+    'DB_USER' => '',
+    'DB_PASSWORD' => '',
+    'driver' => '',
 ];
